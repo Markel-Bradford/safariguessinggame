618eafc243db39b647e89aa901766c6bfa637452
Markel-Bradford <markel.bradford94@gmail.com>
2024-06-20 16:34:59 -0500

    Updated paths and Error page handling

--- src/Pages/Error.js
+++ src/Pages/Error.js
@@ -1,17 +1,17 @@
 import { ArrowUturnLeftIcon, HomeIcon } from '@heroicons/react/24/solid';
-import React from 'react'
-import { useRouteError, Link, useNavigate } from 'react-router-dom'
-import "./Error.css"
+import React from 'react';
+import { useRouteError, Link, useNavigate } from 'react-router-dom';
+import "./Error.css";
 
 const Error = () => {
   const error = useRouteError();
-  const goBack = useNavigate()
+  const navigate = useNavigate();
 
   return (
     <div className='error'>
       <h1 className='errorMsg'>Whoops! We ran into a problem!</h1>
       <div className='errorBtnContainer'>
-        <button className='btn--err' id='backBtn' onClick={() => goBack(-1)}>
+        <button className='btn--err' id='backBtn' onClick={() => navigate(-1)}>
           <ArrowUturnLeftIcon width={20} />
           <span>Go Back</span>
         </button>
@@ -21,7 +21,7 @@ const Error = () => {
         </Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Error
+export default Error;
